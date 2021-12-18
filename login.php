34b26c82f235e9a72ad25acaca5184c220daa894
Kiran Kumar <kirankumar021097@gmail.com>
2021-12-18 18:24:08 +0530

    V1

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div style="width:40%;border:1px solid black;padding-bottom:10px">
+        <center>
+            <h2>Login</h2>
+        
+        <table>
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email" id=""></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password" id=""></td>
+            </tr>
+        </table>
+        <p>Don't have account? <a href="signup.php">Sign Up Here</a></p>
+        <input type="submit" value="Login">
+        </center>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <div style="width:40%;border:1px solid black;padding-bottom:10px">
+        <center>
+            <h2>Sign Up</h2>
+        
+        <table>
+            <tr>
+                <td>Name</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+            <tr>
+                <td>Mobile</td>
+                <td><input type="number" name="number" id=""></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email" id=""></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password" id=""></td>
+            </tr>
+        </table>
+        <p>Already have account? <a href="login.php">Login Here</a></p>
+        <input type="submit" value="Sign Up">
+        </center>
+    </div>
+</body>
+</html>
